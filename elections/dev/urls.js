548212579e2bfdcc5a9ff86818be448e51925704
Nick Baliesnyi <nikita.balesny@gmail.com>
2019-09-06 22:02:42 +0300

    update urls

--- elections/dev/urls.js
+++ elections/dev/urls.js
@@ -1,4 +1,4 @@
-const VOTES      = 'https://gist.githubusercontent.com/nick-baliesnyi/c74265c7301d85a1e6f37cd02d57bbb3/raw/ce07d49c9425989f42e9777536627a498dbb827f/denvelopes.json'
+const VOTES      = 'https://gist.githubusercontent.com/nick-baliesnyi/14d099e1cc35906c635ba8486bce1d81/raw/f63b3e3101f590141ab0335bad8618a10741c0fc/denvelopes1.json'
 
 const CANDIDATES = 'https://gist.githubusercontent.com/nick-baliesnyi/f11586b0597e33430de0d1a12ca17570/raw/e516838e498b03aa5d68b8dafb9dfbb4a2d3f627/candidates.json'
 
